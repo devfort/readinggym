8bb9bd7705beac8cb2757ac17ffb772081737e33
Adam Sven Johnson <adam@pkqk.net>
2013-10-07 19:36:16 +0100

    access reading regulator at reading/practice

--- readfast/urls.py
+++ readfast/urls.py
@@ -4,6 +4,6 @@ import readfast.views
 
 urlpatterns = patterns('',
     url(r"^$",  readfast.views.IndexView.as_view(), name="index"),
-    url(r"^read/$",  readfast.views.ReadView.as_view(), name="read"),
     url(r"^speed/$",  readfast.views.SpeedTestView.as_view(), name="speed-test"),
+    url(r"^reading/practice/$",  readfast.views.PracticeReadingView.as_view(), name="practice"),
 )
