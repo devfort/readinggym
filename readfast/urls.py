86f757b30ad674d2c95cef4928d03cc7d6de089e
David Brownlee <abs@cowlite.local>
2013-10-07 18:19:52 +0100

    Initial speedtest page and form - just enough to manually enter seconds and read back. Need to include wordcount and store rate in cookie

--- readfast/urls.py
+++ readfast/urls.py
@@ -4,4 +4,5 @@ import readfast.views
 
 urlpatterns = patterns('',
     url(r"^read/$",  readfast.views.ReadView.as_view(), name="read"),
+    url(r"^speedtest/$",  readfast.views.SpeedTestView.as_view(), name="speedtest"),
 )
--- readfast/views.py
+++ readfast/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import View
-from django.views.generic import TemplateView
+from django.views.generic import FormView, TemplateView, View
+# import readfast.forms
 
+from readfast.forms import SpeedTestForm
 
 class ReadView(TemplateView):
     template_name = "read.html"
@@ -19,3 +20,27 @@ class ReadView(TemplateView):
         context = super(ReadView, self).get_context_data(**kwargs)
         context['words_to_read'] = words_to_read
         return context
+
+class SpeedTestView(FormView):
+    template_name = "speedtest.html"
+    form_class = SpeedTestForm
+    success_url = 'FIXME-Now-go-test'
+
+    def get_context_data(self, **kwargs):
+        data = open("corpae/makers_snippit.txt")
+        words_to_read = []
+        for word in data.read().split():
+            words_to_read.append("<span>%s </span>" % word)
+        words_to_read = "".join(words_to_read)
+        context = super(SpeedTestView, self).get_context_data(**kwargs)
+        context['words_to_read'] = words_to_read
+        context['wordcount'] = words_to_read.count
+        return context
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        import pdb; pdb.set_trace()
+        print form.cleaned_data['seconds']
+        return super(SpeedTestView, self).form_valid(form)
+
