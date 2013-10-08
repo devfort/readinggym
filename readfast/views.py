b6b4053df3b89eaf2fc7dd3ff57163f0833ca348
David Thompson <mail@fatbusinessman.com>
2013-10-08 11:57:39 +0100

    Rename `corpae` directory to `corpora`.
    Because words.

--- readfast/views.py
+++ readfast/views.py
@@ -36,7 +36,7 @@ class DashboardView(TemplateView):
 
 class ReadViewMixin(object):
     def get_context_data(self, **kwargs):
-        data = open("corpae/makers_snippit.txt")
+        data = open("corpora/makers_snippit.txt")
         words_to_read = spanify(data.read())
 
         context = super(ReadViewMixin, self).get_context_data(**kwargs)
