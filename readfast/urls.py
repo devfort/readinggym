8dcd812c690afa51c41ec8cb7eefe645f5a1fe9e
Alex Stapleton <alexs@prol.etari.at>
2013-10-08 11:48:09 +0100

    Tweak urls

--- readfast/urls.py
+++ readfast/urls.py
@@ -4,11 +4,9 @@ import readfast.views
 
 urlpatterns = patterns('',
     url(r"^$",  readfast.views.IndexView.as_view(), name="index"),
-    url(r"^speed/$",  readfast.views.SpeedTestView.as_view(), name="speed-test"),
+    url(r"^reading/speed/$",  readfast.views.SpeedTestView.as_view(), name="speed-test"),
     url(r"^reading/practice/$",  readfast.views.PracticeReadingView.as_view(), name="practice"),
     url(r"^reading/practice/(?P<pk>[0-9]+)/$",  readfast.views.PracticeReadingView.as_view(), name="practice"),
-    url(r"^compr/(?P<pk>[0-9]+)/$",
-        readfast.views.ComprehensionView.as_view(), name="comprehension"),
-    url(r"^compr/(?P<pk>[0-9]+)/$",
+    url(r"^reading/qs/(?P<pk>[0-9]+)/$",
         readfast.views.ComprehensionView.as_view(), name="comprehension"),
 )
