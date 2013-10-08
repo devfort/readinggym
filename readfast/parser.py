1ff71d958558a85466217b97207447ae4a1c1b77
David Thompson <mail@fatbusinessman.com>
2013-10-08 16:01:17 +0200

    Ugly but functional question parsing.

--- readfast/parser.py
+++ readfast/parser.py
@@ -1,6 +1,50 @@
+import re
+
+class Question(object): pass
+
+
+class Answer(object): pass
+
+
 class PieceParser(object):
 
+    TITLE = re.compile('^Title: (.*)$')
+    QUESTION = re.compile('^Question: (.*)$')
+    ANSWER = re.compile('^A(?P<correct>!)?: (?P<text>.*)$')
+
+    TEXT = re.compile('^Text:')
+
     def __init__(self, source):
-        for line in source:
-            if line.startswith("Title: "):
-                self.title = line.split(" ", 1)[1].strip()
+        self.questions = []
+
+        while True:
+            try:
+                line = source.next()
+            except StopIteration:
+                break
+
+            if self.TEXT.match(line):
+                # We're done here: anything else in the source is body text
+                self.text = "".join(source)
+                break
+
+            title_match = self.TITLE.match(line)
+            if title_match:
+                self.title = title_match.group(1)
+                continue
+
+            question_match = self.QUESTION.match(line)
+            if question_match:
+                q = Question()
+                self.questions.append(q)
+                q.text = question_match.group(1)
+                q.answers = []
+                while True:
+                    answer_match = self.ANSWER.match(source.next())
+                    if answer_match:
+                        a = Answer()
+                        a.text = answer_match.group("text")
+                        a.correct = (answer_match.group("correct") is not None)
+                        q.answers.append(a)
+                    else:
+                        break
--- readfast/tests/parser.py
+++ readfast/tests/parser.py
@@ -12,7 +12,36 @@ Title: A Tale Of Otters
 
 Text:
 Something something otters.
-
 """)
         p = PieceParser(source)
         self.assertEqual(p.title, "A Tale Of Otters")
+
+    def test_extracts_text(self):
+        source = StringIO("""
+Title: A Tale Of Otters
+
+Text:
+Something something otters.
+Something something else.
+""")
+        p = PieceParser(source)
+        self.assertEqual(
+            p.text,
+            "Something something otters.\nSomething something else.\n"
+        )
+
+    def test_extracts_question_and_answer(self):
+        source = StringIO("""
+Question: What are otters?
+A!: Delicious
+
+Text:
+Something something otters.
+        """)
+
+        p = PieceParser(source)
+        self.assertEqual(len(p.questions), 1)
+        self.assertEqual(p.questions[0].text, "What are otters?")
+        self.assertEqual(len(p.questions[0].answers), 1)
+        self.assertEqual(p.questions[0].answers[0].text, "Delicious")
+        self.assertTrue(p.questions[0].answers[0].correct)
