bfd4497ccad61f4d75819d0ebb488d0b568e941d
Alex Stapleton <alexs@prol.etari.at>
2013-10-08 15:53:26 +0100

    Pager.

--- readfast/static/js/regulator.js
+++ readfast/static/js/regulator.js
@@ -1,5 +1,33 @@
-function Regulator(article) {
+function Pager(article) {
     this.article = article;
+    this.setup();
+}
+
+Pager.prototype.setup = function () {
+    var self = this;
+    this.article.wrapInner("<div>");
+    this.contentBox = this.article.find("div").first();
+    this.article.css({
+        "overflow": "hidden",
+        "height": "30em"
+    });
+};
+
+Pager.prototype.firstPage = function (cb) {
+    this.contentBox.animate({
+        "margin-top": "0",
+    }, 100, cb);
+}
+
+Pager.prototype.nextPage = function (cb) {
+    this.contentBox.animate({
+        "margin-top": "-=" + this.article.height(),
+    }, 100, cb);
+};
+
+function Regulator(article, pager) {
+    this.article = article;
+    this.pager = pager;
     this.setup();
     this.running = false;
 }
@@ -27,9 +55,7 @@ Regulator.prototype.setup = function() {
         if (offset && lastOffset) {
             if(offset.top != lastOffset.top) {
                 lines.push({
-                    left: firstOffset.left,
                     width: widthSum,
-                    bottom: lastOffset.top + last$e.height(),
                     words: words
                 });
 
@@ -40,7 +66,7 @@ Regulator.prototype.setup = function() {
             }
         }
 
-        words.push(e);
+        words.push($e);
         widthSum += $e.width();
         lastOffset = offset;
         last$e = $e;
@@ -50,10 +76,12 @@ Regulator.prototype.setup = function() {
 };
 
 Regulator.prototype.start = function() {
+    var self = this;
+
     this.running = true;
     this.article.addClass('reading');
     var box = $("<div>").addClass("regulator");
-    $("body").append(box);
+    $("article").append(box);
 
     var lines = this.lines;
     var words = this.article.find("span");
@@ -61,41 +89,60 @@ Regulator.prototype.start = function() {
 
     var wordsPerLine = words.length / lines.length;
     var pixelsPerWord = this.article.width() / wordsPerLine;
-    var wpm = 200;
+    var wpm = 500;
     
     var guideWidth = 6;
     var pixelRate = (wpm/60)*pixelsPerWord;
     var lineNo = 0;
+
+
     var guideLine = function () {
         if(!regulator.running) { return; }
-        var line = lines[lineNo];
-        var guideTime = ((line.width - guideWidth) / pixelRate)* 1000;
-        
-        $(line.words).each(function(i,s) {
-          $(s).addClass('highlight');
-        });
-        if(lineNo > 0) {
-          $(lines[lineNo-1].words).each(function(i,s) {
-            $(s).removeClass('highlight');
-          });
-        }
-        box.css({
-            left: line.left,
-            top: line.bottom,
-            width: guideWidth
-        });
-        box.show();
 
-        regulator.animation = box.animate({
-            left: (line.left + line.width) - guideWidth,
-        }, guideTime, 'linear', function () {
-            lineNo += 1;
-            box.hide();
-            regulator.nextTick = window.setTimeout(guideLine, 100);
-        });
+        var line = lines[lineNo];
+        var firstWordPos = line.words[0].position();
+        var lineLeft = firstWordPos.left;
+        var lineBottom = firstWordPos.top + line.words[0].height();
+
+        var guideTime = ((line.width - guideWidth) / pixelRate) * 1000;
+        
+        var animateGuide = function () {
+            var firstWordPos = line.words[0].position();
+            var lineBottom = firstWordPos.top + line.words[0].height();
+
+            $(line.words).each(function(i,s) {
+                $(s).addClass('highlight');
+            });
+        
+            if(lineNo > 0) {
+                $(lines[lineNo-1].words).each(function(i,s) {
+                    $(s).removeClass('highlight');
+                });
+            }
+
+            box.css({
+                left: lineLeft,
+                top: lineBottom,
+                width: guideWidth
+            }).show();
+
+            regulator.animation = box.animate({
+                left: (lineLeft + line.width) - guideWidth,
+            }, guideTime, 'linear', function () {
+                lineNo += 1;
+                box.hide();
+                regulator.nextTick = window.setTimeout(guideLine, 200);
+            });
+        }
+
+        if(lineBottom > self.article.height()) {
+            self.pager.nextPage(animateGuide);
+        } else {
+            animateGuide();
+        }
     };
 
-    guideLine();
+    this.pager.firstPage(guideLine);
 };
 
 Regulator.prototype.stop = function() {
@@ -108,11 +155,13 @@ Regulator.prototype.stop = function() {
     if(this.animation) {
         this.animation.finish();
     }
+    this.pager.firstPage();
 }
 
 
 $(function() {
-  var regulator = new Regulator($("article"));
+  var pager = new Pager($("article"));
+  var regulator = new Regulator($("article"), pager);
 
   $('#practice').click(function() {
     if(regulator.running) {
