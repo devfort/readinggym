e7e9553c19c08cf86770d38c03703ce4d9c01502
Alex Stapleton <alexs@prol.etari.at>
2013-10-08 10:12:37 +0100

    Remove unrequired clean_wordcount

--- readfast/forms.py
+++ readfast/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 import re
 
+
 class SpeedTestForm(forms.Form):
     seconds = forms.CharField(max_length=10)
-    wordcount = forms.IntegerField(widget=forms.HiddenInput())
+    wordcount = forms.IntegerField(widget=forms.HiddenInput(),
+                                   min_value=1)
 
     def clean_seconds(self):
         seconds = self.cleaned_data['seconds']
@@ -22,9 +24,3 @@ class SpeedTestForm(forms.Form):
             raise forms.ValidationError("A test cannot be completed in no time")
 
         return seconds
-
-    def clean_wordcount(self):
-        wordcount = self.cleaned_data['wordcount']
-        if wordcount < 1:
-            raise forms.ValidationError("A test must have a positive word count")
-        return wordcount
