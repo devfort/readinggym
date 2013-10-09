2faf7395bfd712f60fae839bb4a3ec3f09f865f5
James Coglan <jcoglan@gmail.com>
2013-10-09 18:55:57 +0100

    Fix the JS tests for speed test control UI.

--- static/js/speed.js
+++ static/js/speed.js
@@ -1,39 +1,35 @@
 'use strict';
 
-
 var enhanceTimerModule = function(timerModule, explanation, pager) {
-    var $timerModule        = $(timerModule),
-        $timerButton        = $('<button>Start</button>').attr("id", "control-article"),
-        startInMillis       = 0,
-        finshInMillis       = 0,
-        elapsedTimeInMillis = 0,
-        checkForLastPage    = function() {
-            if (pager.isLastPage()) {
-               $timerButton.text("Done");
-            }
-        };
-    
-    $timerButton.click(function() {
-        var buttonText = $timerButton.text();
-        if (/start/i.test(buttonText)) {
-            startInMillis = Date.now();
-            $timerButton.text("Page Down");
-        }
+  var $timerModule        = $(timerModule),
+      $timerButton        = $('<button>Start</button>').attr("id", "control-article"),
+      startInMillis       = 0,
+      finshInMillis       = 0,
+      elapsedTimeInMillis = 0;
 
-        if (/page down/i.test(buttonText)) {
-            pager.nextPage(checkForLastPage);
-        }
+  $timerButton.click(function() {
+    var buttonText = $timerButton.text();
 
-        if (/done/i.test(buttonText)) {
-            finshInMillis = Date.now();
-            elapsedTimeInMillis = finshInMillis - startInMillis;
-            $timerModule.find('input[name="seconds"]').val(elapsedTimeInMillis / 1000);
-            $timerModule.find('form').submit();
-        }
-    });
+    if (/start/i.test(buttonText)) {
+      startInMillis = Date.now();
+      $timerButton.text("Page Down");
+    }
 
-    $timerModule.find('#timer-control').html($timerButton);
-    $timerModule.find('.explanation').html(explanation);
-    $timerModule.addClass('enhanced');
+    if (/page down/i.test(buttonText)) {
+      pager.nextPage(function() {
+        if (pager.isLastPage()) $timerButton.text("Done");
+      });
+    }
 
+    if (/done/i.test(buttonText)) {
+      finshInMillis = Date.now();
+      elapsedTimeInMillis = finshInMillis - startInMillis;
+      $timerModule.find('input[name="seconds"]').val(elapsedTimeInMillis / 1000);
+      $timerModule.find('form').submit();
+    }
+  });
+
+  $timerModule.find('#timer-control').html($timerButton);
+  $timerModule.find('.explanation').html(explanation);
+  $timerModule.addClass('enhanced');
 };
--- static/tests/specs/speed_spec.js
+++ static/tests/specs/speed_spec.js
@@ -9,52 +9,69 @@ JS.Test.describe("Speed", function() { with(this) {
         </form>\
       </div>')
 
-    this.pager = {}
+    this.pager = {
+      isLastPage: function() {},
+      nextPage:   function() {}
+    }
 
     enhanceTimerModule(".timer", "Explanation", this.pager)
     this.timer = $(".timer")
+    this.button = this.timer.find("button")
   })
 
   after(function() {
     $("#fixture").empty()
   })
 
-  it("injects some buttons", function() { with(this) {
-    assertEqual( 1, timer.find("button.start").length )
-    assertEqual( 1, timer.find("button.next").length )
+  it("injects a start button", function() { with(this) {
+    assertEqual( 1, button.length )
+    assertEqual( "Start", button.text() )
   }})
 
   describe("when the button is clicked in a start state", function() { with(this) {
     before(function() { with(this) {
-      this.button = timer.find("button.start")
       button.click()
     }})
 
     it("changes to a next page button", function() { with(this) {
-      assertEqual("Page Down", timer.find("button").text() )
+      assertEqual("Page Down", button.text() )
     }})
   }})
 
   describe("when the pager is on the last page", function() { with(this) {
     before(function() { with(this) {
-     
-    }})
-      
-    before(function() { with(this) {
-      stub(pager, "isLastPage").returns(true);
-      this.form = timer.find('form')[0]
-      stub(form, "submit")
+      stub(pager, "isLastPage").returns(true)
+      stub(pager, "nextPage").yields([])
     }})
 
-    it("populates the 'seconds' field", function() { with(this) {
+    it("puts you in the 'page down' state if you click once", function() { with(this) {
       button.click()
-      assertMatch( /^[0-9.]+$/, timer.find("[name=seconds]").val() )
+      assertEqual( "Page Down", button.text() )
     }})
 
-    it("submits the form", function() { with(this) {
-      expect(form, "submit")
+    it("puts you in the 'done' state if you click twice", function() { with(this) {
       button.click()
+      button.click()
+      assertEqual( "Done", button.text() )
     }})
 
+    describe("and you've clicked twice", function() { with(this) {
+      before(function() { with(this) {
+        this.form = timer.find('form')[0]
+        stub(form, "submit")
+        button.click()
+        button.click()
+      }})
+
+      it("populates the 'seconds' field", function() { with(this) {
+        button.click()
+        assertMatch( /^[0-9.]+$/, timer.find("[name=seconds]").val() )
+      }})
+
+      it("submits the form", function() { with(this) {
+        expect(form, "submit")
+        button.click()
+      }})
+    }})
   }})
 }})
