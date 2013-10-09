b450d12fc8985dd7af2baee71ec1a4db37eacba5
rnalexander <rnalexander@gmail.com>
2013-10-09 16:18:57 +0100

    Make it all work as one button.

--- static/js/speed.js
+++ static/js/speed.js
@@ -1,30 +1,39 @@
 'use strict';
 
-var makeNextPageButton = function(article) {
-    var pager = new Pager(article);
-    var nextPageButton = $('<button class="next">Next page</button>');
-    nextPageButton.click(function () {
-        pager.nextPage();
-    });
-    return nextPageButton;
-};
 
-var enhanceTimerModule = function(timerModule, explanation) {
-    timerModule = $(timerModule);
-    var $timing = $('<button class="start">Start</button>');
+var enhanceTimerModule = function(timerModule, explanation, pager) {
+    var $timerModule        = $(timerModule),
+        $timerButton        = $('<button>Start</button>'),
+        startInMillis       = 0,
+        finshInMillis       = 0,
+        elapsedTimeInMillis = 0,
+        checkForLastPage    = function() {
+            if (pager.isLastPage()) {
+               $timerButton.text("Done");
+            }
+        };
     
-    $timing.one("click", function() {
-        var t = Date.now();
-        $timing.one("click", function() {
-            t = Date.now() - t;
-            timerModule.find('input[name="seconds"]').val(t / 1000);
-            timerModule.find('form').submit();
-        }).text("Done");
+    $timerButton.click(function() {
+        var buttonText = $timerButton.text();
+        if (/start/i.test(buttonText)) {
+            startInMillis = Date.now();
+            $timerButton.text("Page Down");
+        }
+
+        if (/page down/i.test(buttonText)) {
+            pager.nextPage(checkForLastPage);
+        }
+
+        if (/done/i.test(buttonText)) {
+            finshInMillis = Date.now();
+            elapsedTimeInMillis = finshInMillis - startInMillis;
+            $timerModule.find('input[name="seconds"]').val(elapsedTimeInMillis / 1000);
+            $timerModule.find('form').submit();
+        }
     });
 
-    timerModule.find('#timer-control').html($timing);
-    timerModule.find('.explanation').html(explanation);
-    timerModule.addClass('enhanced');
+    $timerModule.find('#timer-control').html($timerButton);
+    $timerModule.find('.explanation').html(explanation);
+    $timerModule.addClass('enhanced');
 
-    $timing.before(makeNextPageButton($("article")));
 };
