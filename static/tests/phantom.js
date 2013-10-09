2167235904f7e7dccc68b0c4dca3a68c57523d2e
James Coglan <jcoglan@gmail.com>
2013-10-09 11:59:50 +0100

    Move tests into ./static.

--- static/tests/phantom.js
+++ static/tests/phantom.js
@@ -2,4 +2,4 @@ phantom.injectJs('node_modules/jstest/jstest.js')
 
 var reporter = new JS.Test.Reporters.Headless({})
 
-reporter.open('readfast/static/tests/browser.html')
+reporter.open('static/tests/browser.html')
