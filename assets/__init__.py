35d7eff19f3f52c9cf49a82fd7d2c72e9f7df246
James Coglan <jcoglan@gmail.com>
2013-10-10 23:31:08 +0100

    Inline assets for mobiles.

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,13 +1,22 @@
 # assets/__init__.py
 
 from django.conf import settings
+from ua_parser import user_agent_parser
+
+MOBILES = ['iPhone', 'iPad', 'Kindle']
+
+def is_mobile(request):
+    user_agent = user_agent_parser.Parse(request.META['HTTP_USER_AGENT'])
+    return user_agent['device']['family'] in MOBILES
 
 class AssetsMiddleware:
     def process_request(self, request):
+        options = {}
         if hasattr(settings, 'ASSET_HOSTS'):
-            request.assets = settings.WAKE_ASSETS.renderer(hosts=settings.ASSET_HOSTS)
-        else:
-            request.assets = settings.WAKE_ASSETS.renderer()
+            options['hosts'] = settings.ASSET_HOSTS
+        if is_mobile(request):
+            options['inline'] = True
+        request.assets = settings.WAKE_ASSETS.renderer(**options)
 
 def assets_context(request):
     return {'assets': request.assets}
