f27227f776a59cdee0052714df0d17a28e0c27f4
David Thompson <mail@fatbusinessman.com>
2013-10-10 15:30:32 +0100

    Add initial registration form.
    It uses Django's default markup, which may or may not be acceptable to
    us.

--- speedreader/urls.py
+++ speedreader/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 import readfast.views
+import accounts.views
 
 
 admin.autodiscover()
@@ -9,6 +10,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('readfast.urls')),
+    url(r"^accounts/register/$", accounts.views.RegisterView.as_view(), name="register"),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
